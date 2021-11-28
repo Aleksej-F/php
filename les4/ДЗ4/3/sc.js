2bb69be420ce1588701a287243c63a2b8a76f8c4
AlesejF <l7933@yandex.ru>
2021-11-28 17:12:36 +0300

    dz_4

--- les4/ДЗ4/3/sc.js
+++ les4/ДЗ4/3/sc.js
@@ -2,16 +2,11 @@
 
 window.onload = ()=>{
     const butt1 = document.querySelectorAll('.container-blok');
-    console.log( 'fggggg')
-    console.log( butt1)
     for (let index = 0; index < butt1.length; index++) {
         butt1[index].addEventListener('click', createModals);
-        
     }
-    
 }
 
-     //
 
 
 document.head.insertAdjacentHTML("afterbegin", '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">');
@@ -19,34 +14,42 @@ document.head.insertAdjacentHTML("afterbegin", '<link rel="stylesheet" href="htt
 function createModals (e) { // отрисовка окна
     console.log( 'клик')
     e.stopPropagation();
+    console.log( e.target.offsetLeft)
+    console.log( e.target.offsetTop)
     console.log( e.target.getAttribute('data'))
     let data = e.target.getAttribute('data');
     
     let modals = `
-        <div class="modals-win">
+        
             <div class="modals-w">
                 <div class="modals">
-                    <img src="./big/${data}" alt="" width='600'>
+                    <img src="./big/${data}" alt="" width='600' height='500px'>
                     <div class="closed-captioning" >
                         <i  class="fas fa-times" ></i>
                     </div>
                 </div>
                 
             </div>
-        </div>
+        
         `
     
     document.getElementById('catalog').insertAdjacentHTML("afterbegin", modals);
-
+    
+    
+   
     let closed = document.querySelector('.closed-captioning');
-          
+    let modal = document.querySelector('.modals-w')
+    modal.style.top = `${70+e.target.offsetTop}px`  
+    modal.style.left = `${70+e.target.offsetLeft}px`
     closed.addEventListener('click', setClosedModals);
     setTimeout(() => {
-        document.querySelector('.modals-w').classList.add('active')
+        modal.classList.add('active')
+        modal.style.top = '20px'  
+        modal.style.left = `25vw` 
     }, 100)
-    //document.querySelector('.modals-w').classList.add('active');
+   
 }
 
 const setClosedModals = function() {
-    document.querySelector('.modals-win').remove()
+    document.querySelector('.modals-w').remove()
 }
